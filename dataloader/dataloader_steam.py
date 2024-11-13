160a87aa49ed464bb01ebd4538336b83914f444a
main <wangashley1210@gmail.com>
2024-11-13 15:19:14 +0800

    添加数据加载功能，支持加载完整数据集并更新数据分析笔记本

--- dataloader/dataloader_steam.py
+++ dataloader/dataloader_steam.py
@@ -11,10 +11,16 @@ def load_data(data_path = 'dataset/steam_reviews'):
 
     return train_loader, dev_loader, test_loader
 
+def load_all_data(data_path = 'dataset/steam_reviews'):
+    data_path = data_path + '/steam_reviews_constructiveness_1.5k.csv'
+
+    data_set = pd.read_csv(data_path)
+
+    return data_set
+
 
 if __name__ == "__main__":
     train_loader, dev_loader, test_loader = load_data('dataset/steam_reviews')
-    print(train_loader.head())
-    print(dev_loader.head())
-    print(test_loader.head())
+    data_set = load_all_data('dataset/steam_reviews')
     print(train_loader.shape, dev_loader.shape, test_loader.shape)
+    print(data_set.shape)
--- dataloader/dataloader_youtube_spam.py
+++ dataloader/dataloader_youtube_spam.py
@@ -11,10 +11,16 @@ def load_data(data_path = 'dataset/youtube_spam'):
 
     return train_loader, dev_loader, test_loader
 
+def load_all_data(data_path = 'dataset/youtube_spam'):
+    data_path = data_path + '/Youtube-Spam-Dataset.csv'
+
+    data_set = pd.read_csv(data_path)
+
+    return data_set
 
 if __name__ == "__main__":
     train_loader, dev_loader, test_loader = load_data('dataset/youtube_spam')
-    print(train_loader.head())
-    print(dev_loader.head())
-    print(test_loader.head())
+
+    data_set = load_all_data('dataset/youtube_spam')
     print(train_loader.shape, dev_loader.shape, test_loader.shape)
+    print(data_set.shape)
