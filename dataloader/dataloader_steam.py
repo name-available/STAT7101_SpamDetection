de869f64bc4b715d8bc55304d1c4d688d45af29f
main <wangashley1210@gmail.com>
2024-11-13 21:18:11 +0800

    添加Youtube和Steam评论数据集加载功能，重构数据加载逻辑

--- dataloader/dataloader_steam.py
+++ dataloader/dataloader_steam.py
@@ -1,5 +1,17 @@
 import pandas as pd
 
+class SteamReviewsDataset:
+    def __init__(self, data_path = 'dataset/steam_reviews'):
+        self.data_path = data_path
+        self.train_loader, self.dev_loader, self.test_loader = load_data(data_path)
+        self.data_set = load_all_data(data_path)
+
+    def load_data(self):
+        return self.train_loader, self.dev_loader, self.test_loader
+
+    def load_all_data(self):
+        return self.data_set
+
 def load_data(data_path = 'dataset/steam_reviews'):
     testset_path = data_path + '/train_dev_test_split/test.csv'
     devset_path = data_path + '/train_dev_test_split/dev.csv'
@@ -20,7 +32,9 @@ def load_all_data(data_path = 'dataset/steam_reviews'):
 
 
 if __name__ == "__main__":
-    train_loader, dev_loader, test_loader = load_data('dataset/steam_reviews')
-    data_set = load_all_data('dataset/steam_reviews')
+    steam_dataset = SteamReviewsDataset()
+    train_loader, dev_loader, test_loader = steam_dataset.load_data()
+    data_set = steam_dataset.load_all_data()
+    
     print(train_loader.shape, dev_loader.shape, test_loader.shape)
     print(data_set.shape)
--- dataloader/dataloader_youtube_spam.py
+++ dataloader/dataloader_youtube_spam.py
@@ -1,4 +1,18 @@
 import pandas as pd
+import torch
+
+class YoutubeSpamDataset:
+    def __init__(self, data_path = 'dataset/youtube_spam'):
+        self.data_path = data_path
+        self.train_loader, self.dev_loader, self.test_loader = load_data(data_path)
+        self.data_set = load_all_data(data_path)
+
+    def load_data(self):
+        return self.train_loader, self.dev_loader, self.test_loader
+
+    def load_all_data(self):
+        return self.data_set
+
 
 def load_data(data_path = 'dataset/youtube_spam'):
     testset_path = data_path + '/train_dev_test_split/test.csv'
@@ -19,8 +33,9 @@ def load_all_data(data_path = 'dataset/youtube_spam'):
     return data_set
 
 if __name__ == "__main__":
-    train_loader, dev_loader, test_loader = load_data('dataset/youtube_spam')
+    utb_dataset = YoutubeSpamDataset()
+    train_loader, dev_loader, test_loader = utb_dataset.load_data()
+    data_set = utb_dataset.load_all_data()
 
-    data_set = load_all_data('dataset/youtube_spam')
     print(train_loader.shape, dev_loader.shape, test_loader.shape)
     print(data_set.shape)
