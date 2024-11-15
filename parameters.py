b00175d2f1120a4368a5d56eddf3836a6d388ae5
main <wangashley1210@gmail.com>
2024-11-15 20:24:55 +0800

    新增命令行参数以支持数据路径

--- parameters.py
+++ parameters.py
@@ -3,10 +3,20 @@ import argparse
 
 def get_parameters():
     args = argparse.ArgumentParser('Training and Evaluation script', add_help=False)
+    # data
+    args.add_argument('--data_path', default='dataset/youtube_spam', type=str)
+    args.add_argument('--batch_size', default=32, type=int)
+
+    # model
+    args.add_argument('--best_model_path', default='checkpoints/best_lr_model.pth', type=str)
+    args.add_argument('--text_embedding_dim', default=384, type=int)
+
+    # training
     args.add_argument('--epochs', default=500, type=int)
     args.add_argument('--eval_per_epochs', default=50, type=int)
+
+    # logging
     args.add_argument('--log_file_name', default="LogisticRegression-steam.log", type=str)
-    args.add_argument('--best_model_path', default='checkpoints/best_lr_model.pth', type=str)
 
     return args.parse_args()
 
