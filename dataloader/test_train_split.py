eda5e426467104223ec7a380c5dcbf6460b8b09d
wang-ssh <3509368608@qq.com>
2024-11-23 15:10:26 +0800

    更新数据加载路径，新增 HAN 模型实现及相关功能，调整数据集划分比例

--- dataloader/test_train_split.py
+++ dataloader/test_train_split.py
@@ -2,12 +2,12 @@ import os
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
-split_path = 'dataset/youtube_spam/train_dev_test_split'
+split_path = '/userhome/cs2/wang1210/STAT7101_spam_detection/dataset/youtube_spam/train_dev_test_split'
 os.makedirs(split_path, exist_ok=True)
 
-data = pd.read_csv('dataset/youtube_spam/preprocessed/Processed_Youtube_Spam_Dataset.csv')
+data = pd.read_csv('/userhome/cs2/wang1210/STAT7101_spam_detection/dataset/youtube_spam/preprocessed/Processed_Youtube_Spam_Dataset.csv')
 
-train_data, temp_data = train_test_split(data, test_size=0.4, random_state=42)
+train_data, temp_data = train_test_split(data, test_size=0.2, random_state=42)
 
 val_data, test_data = train_test_split(temp_data, test_size=0.5, random_state=42)
 
